fdc0543cc2f405022a2d42c14336c0f79a85b1f8
dee.randel <dee.randel@gmail.com>
2019-11-23 14:42:46 -0700

    created weapon selector

---
+++ MonsterHunterCodex/MonsterHunterCodex/Vue/Components/weaponSelector.js
@@ -0,0 +1,24 @@
+﻿var template = `
+<div class="wepon-selector">
+Test
+</div>
+
+`;
+
+Vue.component('weapon-selector', {
+    template: template,
+    props: [''],
+    data: function () {
+        return {
+         
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+
+
+});
