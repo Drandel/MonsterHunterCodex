676f41844de1293e8275dbe0faddfa158d0f7548
Dean Randel <dee.randel@gmail.com>
2022-03-16 20:17:11 -0600

    work on rise armory

---
+++ MonsterHunterCodex/MonsterHunterCodex/Vue/Components/gearSelection.js
@@ -0,0 +1,46 @@
+﻿var template = `
+<div class="gear-selector">
+    <div class="data-container" @mouseenter="flags.showEdit=true" @mouseleave="flags.showEdit=false">
+        <div v-bind:class="showEdit" class="data-container-header text-center">
+            <button class="btn-dark">Armor</button>
+            <button class="btn-dark">Weapon</button>
+        </div>
+        <div>
+            <gear-tile ref="headComponent" geartype="head"></gear-tile>
+            <gear-tile ref="chestComponent" geartype="chest"></gear-tile>
+            <gear-tile ref="glovesComponent" geartype="gloves"></gear-tile>
+            <gear-tile ref="waistComponent" geartype="waist"></gear-tile>
+            <gear-tile ref="legsComponent" geartype="legs"></gear-tile>
+            <div v-bind:class="showEdit" class="data-container-footer text-center">
+                <button class="btn btn-dark">Clear</button>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+
+`;
+
+
+
+Vue.component('gear-selection', {
+    template: template,
+    props: [''],
+    data: function () {
+        return {
+            flags: {
+                showEdit: false,
+            },
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+        showEdit: function () {
+            return this.flags.showEdit == true ? "_show" : "_hide";
+        },
+    }
+
+
+});
